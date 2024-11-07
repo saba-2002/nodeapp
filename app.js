62fa9de625563f69303b3466748005b7726319ac
saba-2002 <saba.khan@appinventiv.com>
2024-11-07 15:06:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // app.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('Its Saba, heheh :)');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on http://0.0.0.0:3000');
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Server is running on http://0.0.0.0:3001');
 });
